443277c4aefe6dd8dd47be8497660e093c18e4c6
David-McNiven <davidmcniven@bell.net>
2016-09-29 16:43:04 -0400

    Project Creation

---
+++ Sharp Mail Order - Sales Bonus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Sharp Mail Oder - Sales Bonus	
+// Created by David McNiven
+// Student # 200330143	
+// Created on September 30th 2016	
+// App description
+
+namespace Sharp_Mail_Order___Sales_Bonus
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
